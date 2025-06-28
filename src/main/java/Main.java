a45ef1fb12631ad4066be8ed7d5df7a84204d100
Marie Ch <zima04@mail.ru>
2025-06-28 14:17:08 +0300

    доп дз

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,36 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        int a = random.nextInt(100); // случайное число от 0 до 99
-        int b = random.nextInt(100);
+        int a, b, c;
+        float f;
+        double d;
+        a = 10;
+        b = 20;
+        c = 20;
+        f = 2.5F;
 
-        System.out.println("Сгенерированные числа: a = " + a + ", b = " + b);
+        System.out.println("Арифметические операции");
+        System.out.println("Результат сложения a + b = " + (a + b));
+        System.out.println("Результат вычитания b - a = " + (b - a));
+        System.out.println("Результат умножения a * b = " + (a * b));
+        System.out.println("Результат деления b / a = " + (b / a));
+        System.out.println("Результат сложения a + f = " + (a + f));
+        System.out.println("Результат деления a / f = " + (a / f) + "\n");
 
-        // Арифметические операции с двумя int
-        System.out.println("Сложение: " + (a + b));
-        System.out.println("Вычитание: " + (a - b));
-        System.out.println("Умножение: " + (a * b));
-        System.out.println("Деление: " + (b != 0 ? (a / b) : "деление на ноль"));
-        System.out.println("Остаток от деления: " + (b != 0 ? (a % b) : "деление на ноль"));
+        System.out.println("Логические операции");
+        System.out.println("b больше чем a? " + (a > b));
+        System.out.println("b меньше или равно чем a? " + (a <= b));
+        System.out.println("b равно с? " + (b == c));
+        System.out.println("b больше или равно с? " + (b >= c) + "\n");
 
-        // Арифметические операции с int и double
-        double c = random.nextDouble() * 10; // случайное число от 0 до 10
-        System.out.println("int + double: " + (a + c));
-        System.out.println("int - double: " + (a - c));
-        System.out.println("int * double: " + (a * c));
-        System.out.println("int / double: " + (a / c));
+        f = Float.MAX_VALUE;
+        d = Double.MAX_VALUE;
+        System.out.println("Переполнение при арифметической операции");
+        System.out.println("Переполненное значение float: " + (f * 2));
+        System.out.println("Переполненное значение double: " + (d * 2));
+        System.out.println("Результат деления 0.0/0.0: " + (0.0 / 0.0));
+        f = Float.MIN_VALUE;
+        System.out.println("Минимальное значение float = " + f);
+        System.out.println("Результат деления минимального значения float на 10 = " + (f / 10));
 
-        // Логические операции
-        System.out.println("a > b: " + (a > b));
-        System.out.println("a < b: " + (a < b));
-        System.out.println("a >= b: " + (a >= b));
-        System.out.println("a <= b: " + (a <= b));
-        System.out.println("a == b: " + (a == b));
-        System.out.println("a != b: " + (a != b));
-
-        // Диапазоны чисел с плавающей точкой
-        System.out.println("Максимальное значение float: " + Float.MAX_VALUE);
-        System.out.println("Минимальное значение float: " + Float.MIN_VALUE);
-        System.out.println("Максимальное значение double: " + Double.MAX_VALUE);
-        System.out.println("Минимальное значение double: " + Double.MIN_VALUE);
-
-        // Переполнение
-        int maxInt = Integer.MAX_VALUE;
-        System.out.println("Максимальный int: " + maxInt);
-        System.out.println("Переполнение int: " + (maxInt + 1)); // Уйдет в отрицательные значения
-
-        float largeFloat = Float.MAX_VALUE;
-        System.out.println("Переполнение float: " + (largeFloat * 2)); // Получим бесконечность
-
-        double largeDouble = Double.MAX_VALUE;
-        System.out.println("Переполнение double: " + (largeDouble * 2)); // Получим бесконечность
-
-        System.out.println("Проверка бесконечности float: " + Float.isInfinite(largeFloat * 2));
-        System.out.println("Проверка бесконечности double: " + Double.isInfinite(largeDouble * 2));
     }
 }
